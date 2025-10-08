f7d2a64fc6ec4c099356b6083bdd82a7d3739841
Grady Harwood <gradyharwood@gmail.com>
2025-10-08 15:33:41 -0400

    lmao

--- app/redirect/page.tsx
+++ app/redirect/page.tsx
@@ -87,7 +87,7 @@ export default function RedirectPage() {
         }
       }
 
-      trackAnalytics().then(() => { redirect() }).catch((e) => console.log('Analytics tracking failed :: ' + e)
+      trackAnalytics().then(() => { redirect() }).catch((e) => console.log('Analytics tracking failed :: ' + e))
 
     } catch (err) {
       console.error('Redirect client error', err)
